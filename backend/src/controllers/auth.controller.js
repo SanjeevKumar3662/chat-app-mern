aeadf533b979118848d02807184a21c001138399
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-13 16:21:53 +0530

    add update profile function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/user.model.js";
 import { generateToken } from "../utils/utils.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -99,5 +100,38 @@ export const logout = async (req, res) => {
   } catch (error) {
     console.log("error in logout auth controller : ", error);
     return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({
+        message: "Profile Pic is required ",
+      });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    console.log("cloudinary upload response : ", uploadResponse);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      updatedUser,
+    });
+  } catch (error) {
+    console.log("Error in updateProfile auth controller : ", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
