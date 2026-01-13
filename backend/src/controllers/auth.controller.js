2bffb3486ccdcb9a2cd5420ede54bb3b57dbc096
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-13 16:40:58 +0530

    add checkAuth auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -135,3 +135,14 @@ export const updateProfile = async (req, res) => {
     });
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    return res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller : ", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
