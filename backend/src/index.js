c4cc6c77aa94f6ba5dc07d68f1569efa8885d884
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-13 16:42:49 +0530

    add connectDB function and json,url,cookie middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,22 @@
 import { configDotenv } from "dotenv";
 configDotenv();
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./db/db.js";
 
 const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on", PORT);
+  connectDB();
 });
