5d8b4302d63dfa39a595f67d8cd6c0b01612e6d9
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-13 12:56:16 +0530

    add logout auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -87,4 +87,17 @@ export const login = async (req, res) => {
     });
   }
 };
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAge: "0",
+    });
+
+    return res.status(200).json({
+      message: "Logout successfully",
+    });
+  } catch (error) {
+    console.log("error in logout auth controller : ", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
