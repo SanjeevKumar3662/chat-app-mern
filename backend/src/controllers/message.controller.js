6ebca91aeeeb8bf447a6d98f30f83ff8c1c603eb
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-14 10:58:30 +0530

    add sendMessage message controller without socket.io functionality

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import { Message } from "../models/message.model.js";
 import { User } from "../models/user.model.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -32,6 +33,37 @@ export const getMessages = async (req, res) => {
     return res.status(200).json(messages);
   } catch (error) {
     console.log("error in getMessages message controller :", error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = cloudinary.uploader.upload(image);
+      imageUrl = (await uploadResponse).secure_url;
+    }
+
+    const newMessge = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    // a todo : add socket.io here
+
+    return res.status(201).json(newMessge);
+  } catch (error) {
+    console.log("error in sendMessage message controller :", error);
 
     return res.status(500).json({
       message: "Internal Server Error",
