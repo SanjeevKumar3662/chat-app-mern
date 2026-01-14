c62f5065469dc58cda8ce3a0870e530cc7341435
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-14 10:59:04 +0530

    add message route file with message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./db/db.js";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on", PORT);
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
+
+const router = Router();
+
+router.get("/user", protectedRoute, getUsersForSidebar);
+router.get("/:id", protectedRoute, getMessages);
+router.post("/send/:id", protectedRoute, sendMessage);
+
+export default router;
