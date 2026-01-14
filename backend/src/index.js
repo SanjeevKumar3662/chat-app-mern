cd36989e1d8b69426388b35f7289fad549b67c06
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-14 16:39:18 +0530

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import { configDotenv } from "dotenv";
 configDotenv();
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -14,6 +15,12 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
