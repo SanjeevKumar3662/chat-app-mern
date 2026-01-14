33d56ce6f8ef35a2252a34b027323721bae8a091
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-14 10:44:57 +0530

    add getMessages controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import { Message } from "../models/message.model.js";
 import { User } from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
@@ -9,6 +10,28 @@ export const getUsersForSidebar = async (req, res) => {
     return res.status(200).json(allUsers);
   } catch (error) {
     console.log("error in getUsersForSidebar message controller :", error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const userToChatId = req.params.id;
+    const myId = req.user.userId;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    return res.status(200).json(messages);
+  } catch (error) {
+    console.log("error in getMessages message controller :", error);
 
     return res.status(500).json({
       message: "Internal Server Error",
