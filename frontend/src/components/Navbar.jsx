7532e58046da755dece6728231996fd79d66901e
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-22 19:37:18 +0530

    add mockup navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { authUser, logout } = useAuthStore();
+
+  console.log("authUser", authUser);
+
+  return (
+    <div className="px-2 py-6 bg-blue-400 flex justify-between text-xl gap-5">
+      <Link to="/">
+        <div>Home</div>
+      </Link>
+      <span>{authUser ? `${authUser.fullName} is loggedIn ` : "Home"}</span>
+
+      {authUser ? (
+        <button
+          className="bg-red-500 rounded-md text-white px-2 py-1"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      ) : (
+        <Link to="/login">
+          <button className=" bg-violet-600 rounded-md" text-white px-2 py-1>
+            login
+          </button>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
