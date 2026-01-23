475910df6370f42575ce97ede03e1a37bf29b92e
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-23 20:52:53 +0530

    feat : add scroll to bottom when messages change

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useEffect } from "react";
 import { useMessageStore } from "../store/useMessageStore";
 import { useAuthStore } from "../store/useAuthStore";
@@ -9,7 +9,13 @@ export const Chat = ({ chatTarget }) => {
   const { authUser } = useAuthStore();
   const { socket } = useSocketStore();
 
-  const [message, setMessage] = useState({ text: "" });
+  const [message, setMessage] = useState({ text: "", image: "" });
+
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    bottomRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   //Load old chat
   useEffect(() => {
@@ -19,16 +25,7 @@ export const Chat = ({ chatTarget }) => {
     })();
   }, [chatTarget, getMessage]);
 
-  // useEffect(() => {
-  //   if (!socket) return;
-  //   socket.on("newMessage", (data) => {
-  //     console.log("newMessage", data);
-  //     (async () => {
-  //       await addMessage(data);
-  //     })();
-  //   });
-  // }, [addMessage, socket]);
-
+  //Socket IO logic
   useEffect(() => {
     if (!socket || !chatTarget) return;
 
@@ -49,15 +46,32 @@ export const Chat = ({ chatTarget }) => {
     };
   }, [socket, chatTarget, authUser._id, addMessage]);
 
-  const handleMessageSubmit = () => {
-    if (message.text === "") return;
+  //for uploading image
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      setMessage((prev) => ({
+        ...prev,
+        image: reader.result, // base64 string
+      }));
+    };
+  };
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    if (message.text === "" && message.image === "") return;
     (async () => {
       await sendMessage(chatTarget._id, message);
-      setMessage({ text: "" });
+      setMessage({ text: "", image: "" });
     })();
   };
   // console.log("messages", messages);
-  console.log("chatTarget", chatTarget);
+  // console.log("chatTarget", chatTarget);
 
   return (
     <section className=" bg-linear-to-r from-slate-900 to-slate-700 flex-3 flex flex-col   justify-between overflow-y-auto">
@@ -73,32 +87,48 @@ export const Chat = ({ chatTarget }) => {
               return (
                 <li
                   key={message._id}
-                  className={` p-2  m-3 rounded-md max-w-1/2 ${
+                  className={` p-2  m-3 rounded-md  ${
                     message.senderId === authUser._id
                       ? "self-end bg-blue-400"
                       : "self-start bg-gray-600 text-white"
                   }`}
                 >
-                  {message.text}
+                  {message.text && message.text}
+                  {message.image && (
+                    <img
+                      src={message.image}
+                      alt="sent"
+                      className="max-w-100 rounded-md mt-1"
+                    />
+                  )}
                 </li>
               );
             })}
         </ul>
+        <div ref={bottomRef}></div>
       </div>
-      <div className="sticky  bottom-0  border p-2 bg-gray-800 flex gap-2">
-        <input
-          onChange={(e) => setMessage({ ...message, text: e.target.value })}
-          value={message.text}
-          className=" flex-4 rounded-md text-white bg-gray-600"
-          type="text"
-        />
-        <button
-          onClick={handleMessageSubmit}
-          className="flex-1 bg-green-400 active:bg-green-300 p-2 rounded-md"
-        >
-          Send
-        </button>
-      </div>
+      <form>
+        <div className="sticky  bottom-0  border p-2 bg-gray-800 flex gap-2">
+          <input
+            onChange={(e) => setMessage({ ...message, text: e.target.value })}
+            value={message.text}
+            className=" flex-4 rounded-md text-white bg-gray-600"
+            type="text"
+          />
+          <input
+            className=" bg-green-300 max-w-50 rounded-md"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+          <button
+            onClick={handleMessageSubmit}
+            className="flex-1 bg-green-400 active:bg-green-300 p-2 rounded-md"
+          >
+            Send
+          </button>
+        </div>
+      </form>
     </section>
   );
 };
