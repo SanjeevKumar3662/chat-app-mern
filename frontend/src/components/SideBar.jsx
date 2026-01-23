13b4f2c47c330d420d28a747ea76cffb97b4a9eb
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-23 14:37:40 +0530

    passing first user object in setChatTarget and add bg styles

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useMessageStore } from "../store/useMessageStore";
+import Navbar from "./Navbar";
 
 export const SideBar = ({ setChatTarget }) => {
   const [users, setUsers] = useState(null);
@@ -10,20 +11,22 @@ export const SideBar = ({ setChatTarget }) => {
     (async () => {
       const data = await getUsers();
       setUsers(data);
+      setChatTarget(data[0]);
     })();
-  }, [getUsers]);
+  }, [getUsers, setChatTarget, users]);
 
   // console.log("users:", users);
   console.log("Sidebar rendered");
   return (
-    <section className="bg-green-200 flex-1  ">
+    <section className="flex-1 bg-linear-to-r from-slate-900 to-slate-700">
+      <Navbar />
       <ul className="flex flex-col p-3 gap-2">
         {users &&
           users.map((user) => {
             return (
               <li
-                onClick={() => setChatTarget(user._id)}
-                className="bg-blue-400 p-2 rounded-md"
+                onClick={() => setChatTarget(user)}
+                className="bg-blue-400 active:bg-blue-300 p-2 rounded-md  cursor-pointer "
                 key={user._id}
               >
                 {user.fullName}
