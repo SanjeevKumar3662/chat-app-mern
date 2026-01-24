caa8f3b63533dd89e5ef517d214f3cc92408700c
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-24 13:00:01 +0530

    add a new backend server uri

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-mern-backend-l5bi.onrender.com",
+    ],
     credentials: true,
   },
 });
